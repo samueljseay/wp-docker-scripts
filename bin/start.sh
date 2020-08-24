eb167c83b067739a38eeba7b10255a1dc417a780
Sam Seay <samueljseay@gmail.com>
2020-08-24 23:10:15 +1200

    Add support for xdebug.

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build ./docker -f ./docker/Dockerfile -t wordpress_installer:latest
+docker build ./docker -f ./docker/wordpress-cli/Dockerfile -t wordpress_installer:latest
+docker build ./docker -f ./docker/wordpress/Dockerfile -t wordpress_debug:latest
 docker-compose -f dev-bindings.yml up --build 

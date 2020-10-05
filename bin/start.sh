3cea6018d8cbc1866df2dd7f514ab1c2ca9433d7
Sam Seay <samueljseay@gmail.com>
2020-10-05 17:02:40 +1300

    Initial commit of dynamic mounts.

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,4 @@
 
 docker build ./docker -f ./docker/wordpress-cli/Dockerfile -t wordpress_installer:latest
 docker build ./docker -f ./docker/wordpress/Dockerfile -t wordpress_debug:latest
-docker-compose up --build 
+docker-compose -f dynamic-compose.yml up --build 
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mappings: {
+    "wp-content/plugins/woocommerce-admin":
+      "/Users/samseay/Code/automattic/woocommerce-admin",
+    "wp-content/plugins/woocommerce":
+      "/Users/samseay/Code/automattic/woocommerce",
+  },
+};

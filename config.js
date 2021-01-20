a2f22ede2bd02b60fc95e8a272bf8b5510c2c775
Sam Seay <samueljseay@gmail.com>
2021-01-20 14:38:55 +1300

    Provide extended example config.

--- config.js
+++ config.js
@@ -1,10 +1,24 @@
 module.exports = {
   mappings: {
-    // example
-    "wp-content/plugins/woocommerce-admin":
-      "path/to/your/local/clone/of/woocommerce-admin",
+    "wp-content/plugins/woocommerce-admin": "/local/path/to/woocommerce-admin",
   },
+  installPlugins: ["woocommerce"],
+  activatePlugins: ["woocommerce"],
   CONTAINER_NAME_PREFIX: "wp_dev",
   WP_HOST_NAME: "http://docker.for.mac.localhost",
   WP_PORT: 1234,
+  wpConfig: {
+    SCRIPT_DEBUG: {
+      value: true,
+      raw: true,
+    },
+    WP_DEBUG_LOG: {
+      value: true,
+      raw: true,
+    },
+    WP_DEBUG_DISPLAY: {
+      value: false,
+      raw: true,
+    },
+  },
 };
